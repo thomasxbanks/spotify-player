e754129799155519f8bae1685b7d5f2d7dc5ff76
Thom Banks <thomas@shoot-the-moon.co.uk>
2016-11-22 16:09:58 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spotify Player
+
+Messing around with the Spotify API to create a playlist previewer.
+
+## Dependencies
+
+1. mustache
+
+## Installation
+
+1. `clone` repository
+1. `npm i`
---
+++ index.js
@@ -0,0 +1,148 @@
+'use strict'
+
+$(document).ready(function() {
+    // Define your variables
+    const user_id = 'thomasxbanks'
+    const playlist_id = '12euo5zUOLXaNvfrfvmHsl'
+
+    // Get publicly available user information
+    $.get('https://api.spotify.com/v1/users/' + user_id, function(data) {
+        // Log for debug
+        //console.log('pre', data)
+
+        // If user does not have a defined avatar, use a generic Spotify logo
+        if (!data.images[0]) {
+            data.images = [{
+                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2000px-Spotify_logo_without_text.svg.png'
+            }]
+        }
+
+        // Create the personalisation
+        $('.user_id').html(data.id)
+        $('img.user_avatar').attr('src', data.images[0].url).attr('alt', data.id)
+        $('a.user_url').attr('href', data.external_urls.spotify)
+
+        // Log for Debug
+        //console.log('post', data)
+    })
+
+
+
+    // Get oAuth playlist data
+
+    $.ajax({
+        url: 'https://api.spotify.com/v1/users/' + user_id + '/playlists/' + playlist_id,
+        context: document.getElementsByTagName('main')[0],
+        crossDomain: true,
+        dataType: "json",
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer BQAkeIA1R_yrsb7RRBc-IlvYEbAK9ROM6ujSwKdKIjzo7Meihn59rJ-kHQr90-NRyWk40h520QanyqqB_tD-wwtJqtubsAmsate_jwPoOqNt4xKtpE69xkTxJvuvb-EmbdqHxZo7sgEESu4P1EwIk31xcfZWBrlvlg'
+        }
+    }).done(function(data) {
+        // Log for debug
+        console.log(data)
+        let playlist = {
+            title: data.name,
+            description: data.description,
+            image: data.images[0].url
+        }
+
+        let dataTracks = data.tracks.items
+        let tracksArray = []
+
+        $.each(dataTracks, function(i, obj) {
+                var t = dataTracks[i].track
+                var trackObj = {
+                    artist: t.artists[0].name,
+                    album: t.album.name,
+                    name: t.name,
+                    art: t.album.images[0].url,
+                    url: t.preview_url
+                }
+                tracksArray.push(trackObj)
+            })
+            // Log for debug
+            //console.info(dataTracks)
+            //console.log(tracksArray)
+
+        // Display the playlist header
+        let playlister = Mustache.render("<div class='playlist_wrapper' style='background-image: url({{image}})'><div><h1>{{title}}</h1></div><div><p>{{description}}</p></div></div>", playlist)
+        $(this).html(playlister)
+
+        // Display each track
+        $.get('track.html', function(templates) {
+            // Fetch the <script /> block from the loaded external
+            // template file which contains our greetings template.
+            var template = $(templates).filter('#trackTemplate').html()
+            $.each(tracksArray, function(i, obj) {
+                let tracktemplate = ''
+                let tracklist = Mustache.render(template, tracksArray[i])
+                $('main').append(tracklist)
+            })
+        })
+
+    })
+
+    let progressIndication
+
+    function startProgress(currentPlayer) {
+        // console.info('start progress', currentPlayer)
+            // progress Indicator
+        let max = $(currentPlayer.progress).attr('max')
+        let i = $(currentPlayer.progress).attr('value')
+        progressIndication = setInterval(function() {
+            $(currentPlayer.progress).attr('value', currentPlayer.audio.currentTime)
+            if ($(currentPlayer.progress).attr('value') >= max) {
+                stopProgress(progressIndication, currentPlayer)
+            }
+        }, 10)
+    }
+
+    function stopProgress(progressIndication, currentPlayer) {
+        // console.info('stop progress')
+        clearInterval(progressIndication)
+        if ($(currentPlayer.progress).attr('value') >= $(currentPlayer.progress).attr('max')) {
+            $(currentPlayer.progress).attr('value', 0)
+            toggleButtonState(currentPlayer, 'play')
+        }
+    }
+
+    function toggleButtonState(currentPlayer, state) {
+        // console.log('toggle', currentPlayer.button[0])
+        $(currentPlayer.button[0]).html(state)
+    }
+
+    $('body').on('click', '.track_wrapper .audio-controls button', function() {
+        let currentPlayer = {
+                button: $(this),
+                audio: $(this).parent().parent().find('audio')[0],
+                progress: $(this).parent().find('progress')[0]
+            }
+            // @TODO: remove after testing
+
+        currentPlayer.audio.volume = 1
+        if (currentPlayer.audio.paused == false) {
+            // If the track is already playing...
+            // console.info('Track is now PAUSED')
+            stopProgress(progressIndication, currentPlayer)
+            currentPlayer.audio.pause()
+            toggleButtonState(currentPlayer, 'play')
+
+        } else {
+            // If the track is currently paused
+            console.info('Track is now PLAYING')
+            toggleButtonState(currentPlayer, 'pause')
+            for (let i = 0; i < $('audio').length; i++) {
+                $('audio')[i].pause()
+                $('button').attr('data-state', 'play').html('play')
+                toggleButtonState(currentPlayer, 'pause')
+            }
+            startProgress(currentPlayer)
+            currentPlayer.audio.play()
+        }
+
+
+    })
+
+})
