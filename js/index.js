355533c3895ce87203ec204f1394c944bca34513
Thom Banks <thomas@shoot-the-moon.co.uk>
2017-01-24 17:30:56 +0000

    a to z artist filtering added. v v basic

--- js/index.js
+++ js/index.js
@@ -14,23 +14,56 @@ $(document).ready(function() {
                         album: t.album.name,
                         name: t.name,
                         art: t.album.images[0].url,
-                        url: t.preview_url
+                        url: t.preview_url,
+                        popularity: t.popularity
                     }
                     tracksArray.push(trackObj)
                 })
                 // log for debug
-            console.log(tracksArray)
+            //console.log(tracksArray)
                 // Display each track
-            $.get('templates/track.html', function(templates) {
-                // Fetch the <script /> block from the loaded external
-                // template file which contains our track template.
-                var template = $(templates).filter('#trackTemplate').html()
-                $.each(tracksArray, function(i, obj) {
-                    let tracktemplate = ''
-                    let tracklist = Mustache.render(template, tracksArray[i])
-                    $('.playlister_wrapper').append(tracklist)
-                })
+            // $.get('templates/track.html', function(templates) {
+            //     // Fetch the <script /> block from the loaded external
+            //     // template file which contains our track template.
+            //     var template = $(templates).filter('#trackTemplate').html()
+            //     $.each(tracksArray, function(i, obj) {
+            //         let tracktemplate = ''
+            //         let tracklist = Mustache.render(template, tracksArray[i])
+            //         $('.playlister_wrapper').append(tracklist)
+            //     })
+            // })
+
+            let MostPopular = (sort) => {
+              tracksArray.map(function(item) {
+                if (item['popularity'] > sort ) {
+                  var trackLayout = '<article class="card-post card-track"><div class="inner"><header><img src="'+item.art+'" /><h2>'+item.artist+'</h2></header><div class="content"><cite><strong>Album: </strong>'+item.album +'<br/><strong>Track: </strong>'+item.name + '</cite><div class="audio-controls"><progress max="30" value="0"></progress><br /><button data-state="play"><i class="icon-play"></i></button></div><audio controls><source src="'+item.url+'" type="audio/mpeg">Your browser does not support the audio element.</audio></div></div></article>'
+                  $('.playlister_wrapper').append(trackLayout)
+
+                }
+              })
+            }
+
+            let SortAZTracks = (sort) => {
+              tracksArray.map(function(item) {
+                console.log(item)
+                if (item['artist'].charAt(0) == sort ) {
+                  var trackLayout = '<article class="card-post card-track"><div class="inner"><header><img src="'+item.art+'" /><h2>'+item.artist+'</h2></header><div class="content"><cite><strong>Album: </strong>'+item.album +'<br/><strong>Track: </strong>'+item.name + '</cite><div class="audio-controls"><progress max="30" value="0"></progress><br /><button data-state="play"><i class="icon-play"></i></button></div><audio controls><source src="'+item.url+'" type="audio/mpeg">Your browser does not support the audio element.</audio></div></div></article>'
+                  $('.playlister_wrapper').append(trackLayout)
+
+                }
+              })
+            }
+
+            MostPopular(70)
+
+
+            $('.filter-az li a').on('click', function(e){
+              e.preventDefault()
+              $('.playlister_wrapper').html("")
+              SortAZTracks($(this).html())
+              $('.drop-down, .drop-down-toggle').removeClass('is-active')
             })
+
         }
     })
     let progressIndication
@@ -63,7 +96,7 @@ $(document).ready(function() {
         $(currentPlayer.button[0]).attr('data-state', state)
     }
 
-    $('body').on('click', '.track_wrapper .audio-controls button', function() {
+    $('body').on('click', '.card-track .audio-controls button', function() {
         let currentPlayer = {
                 button: $(this),
                 audio: $(this).parent().parent().find('audio')[0],
@@ -95,4 +128,20 @@ $(document).ready(function() {
 
     })
     console.log('end')
+})
+
+// DROPDOWN
+$('.drop-down-toggle').on('click', function(e) {
+    let dropdown = $(this)["0"].nextElementSibling
+    if ($(this).hasClass('is-active')) {
+        $(this).removeClass('is-active')
+        $(dropdown).removeClass('is-active')
+    } else {
+        $('.drop-down-toggle.is-active, .drop-down.is-active').toggleClass('is-active')
+        $(this).addClass('is-active')
+        $(dropdown).addClass('is-active')
+    }
+
+
+
 })
